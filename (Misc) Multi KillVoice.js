2487422fac9f09265d7f3a139fd8c2297b4df13e
Zeki <iziquizel@gmail.com>
2020-07-28 00:39:20 -0300

    Implemented MDXGui and Custom Directory support
    In this update the script now has its own menu and now supports custom directory. Futher optimization will be done.
    Planning on making a standalone config system so it doesn't conflict with other scripts that use MDX.

--- (Misc) Multi KillVoice.js
+++ (Misc) Multi KillVoice.js
@@ -1,97 +1,179 @@
-UI.AddLabel("          -- Multi KillVoice --");
-UI.AddLabel("              Made by Zeki");
-UI.AddLabel("");
-UI.AddCheckbox("Enable KillVoice");
-UI.AddSliderInt("Amount", 1, 5);
-UI.AddDropdown("Selected", [1, 2, 3, 4, 5]);
-UI.AddCheckbox("Loopback");
-
-UI.AddSliderFloat("Length", 0.0, 10.0);
-
-/*
-Under Development
-Probably released some time after you see this.
-
-UI.AddCheckbox("Random KillVoice");*/
-
-// textboxes create
-UI.AddTextbox("KillVoice 1");
-UI.AddTextbox("KillVoice 2");
-UI.AddTextbox("KillVoice 3");
-UI.AddTextbox("KillVoice 4");
-UI.AddTextbox("KillVoice 5");
-UI.AddLabel("");
-
-// textboxes hide
-UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", "KillVoice 1", false);
-UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", "KillVoice 2", false);
-UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", "KillVoice 3", false);
-UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", "KillVoice 4", false);
-UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", "KillVoice 5", false);
-
-UI.AddCheckbox("Set Amount");
-UI.AddCheckbox("Reset Amount");
-UI.AddLabel("");
+var MDX = require("mdxlib.js");
 
 var playing = false;
 var started = 0.0;
 
-
+// Get Value
 function getValue(name){
 	return UI.GetValue("Misc", "JAVASCRIPT", "Script items", name);
 }
+// Set value
 function setValue(name, val){
 	UI.SetValue("Misc", "JAVASCRIPT", "Script items", name, val);
 }
-
+// Get String
 function getString(name){
 	UI.GetString("Misc", "JAVASCRIPT", "Script items", name);
 }
-
+// Set Enabled
 function setEnabled(name, val){
 	UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", name, val);
 }
 
-function onFSN(){
-	var selectedVal = getValue("Amount");
-	if(getValue("Set Amount")){
-		for(i=0; i < selectedVal; i++){
-			UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", "KillVoice " + (i+1), true);
+// Function variables
+var enabled = false;
+var arrayopened = false;
+var dropdownopts = ["1", "2", "3", "4", "5"];
+var chosenopt = 0;
+var loopback = false;
+var file1 = ""; var file2 = ""; var file3 = ""; var file4 = ""; var file5 = "";
+var filenames = [];
+var length = [0, 10];
+var custom_directory = false;
+var directory = "";
+
+// Draw
+function draw(){
+
+	var sx = MDX.agx + 150;
+	var sy = MDX.agy + 30;
+
+	r = 251;
+	g = 160;
+	b = 38;
+
+	if(UI.IsMenuOpen()){
+
+		MDX.menu("Multi Killvoice", " by Zeki#0001", MDX.agx, MDX.agy, 500, 300, r, g, b);
+
+		if(MDX.drag(MDX.agx, MDX.agy).x != 200 || MDX.drag(MDX.agx, MDX.agy).y != 200){
+			MDX.agx = MDX.drag(MDX.agx, MDX.agy).x - 150;
+			MDX.agy = MDX.drag(MDX.agx, MDX.agy).y - 10;
 		}
-		setValue("Set Amount", false);
+
+		var settings = new MDX.MDXTab("Settings", true, MDX.agx + 10, MDX.agy + 30);
+		var files = new MDX.MDXTab("Files", false, MDX.agx + 10, MDX.agy + 50);
+		var configtab = new MDX.MDXTab("Config", false, MDX.agx + 10, MDX.agy + 70);
+		var credits = new MDX.MDXTab("Credits", false, MDX.agx + 10, MDX.agy + 90);
+		MDX.tab(settings);
+		MDX.tab(files);
+		MDX.tab(configtab);
+		MDX.tab(credits);
+
+		if(settings.getTabVisibility()){
+			// Enable checkbox
+			if(MDX.checkbox("Enable", sx, sy, enabled))
+				enabled = !enabled;
+
+			// Loopback
+			if(MDX.checkbox("Loopback", sx, sy + 20, loopback))
+				loopback = !loopback;
+			
+			// Select dropdown
+			var dropdown = MDX.dropdown("Selected", sx, sy + 40, dropdownopts, arrayopened, dropdownopts[chosenopt]);
+			if(dropdown != undefined){
+				if(dropdown == "closed"){
+					arrayopened = !arrayopened;
+				} else {
+					chosenopt = dropdown;
+					arrayopened = !arrayopened;
+				}
+			}
+
+			// Length slider
+			length = MDX.slider("Length", sx + 80, sy + 40, length[1], 0, 10, true);
+
+
+		}
+
+		if(files.getTabVisibility()){
+			// Files
+			file1 = MDX.textbox("KillVoice 1", sx, sy, file1);
+			file2 = MDX.textbox("KillVoice 2", sx, sy + 35, file2);
+			file3 = MDX.textbox("KillVoice 3", sx, sy + 70, file3);
+			file4 = MDX.textbox("KillVoice 4", sx, sy + 105, file4);
+			file5 = MDX.textbox("KillVoice 5", sx, sy + 140, file5);
+
+			if(MDX.checkbox("Custom directory", sx, sy + 190, custom_directory)){
+				custom_directory = !custom_directory;
+			}
+			if(custom_directory)
+				directory = MDX.textbox("Directory:", sx, sy + 210, directory);
+		}
+
+		if(configtab.getTabVisibility()){
+			// Save config button
+			if(MDX.button("Save config", sx, sy)){
+				var filenames = [file1, file2, file3, file4, file5];
+				var files = JSON.stringify(filenames);
+				//                 key:        t/f            key           t/f            key          {int}             key        {array}
+				var config = 
+				'{' + 
+					' "enabled":'+ enabled + 
+					', "loopback":' + loopback + 
+					', "selected":' + chosenopt + 
+					', "filenames":' + files + 
+					', "length":' + JSON.stringify(length) + 
+					', "custom_dir":' + custom_directory +
+					', "directory": "' + directory + '"' +
+				'}';
+				MDX.saveconfig(config);
+				Cheat.Print(config);
+			}
+
+			// Load config button
+			if(MDX.button("Load config", sx, sy + 30)){
+				var config = JSON.parse(MDX.loadconfig());
+				enabled  = config.enabled;
+				loopback = config.loopback;
+				chosenopt = config.selected;
+				file1 = config.filenames[0];
+				file2 = config.filenames[1];
+				file3 = config.filenames[2];
+				file4 = config.filenames[3];
+				file5 = config.filenames[4];
+				length[0] = config.length[0];
+				length[1] = config.length[1];
+				custom_directory = config.custom_dir;
+				directory = config.directory;
+			}
+		}
+
+		if(credits.getTabVisibility()){
+			var fonte = Render.AddFont("Tahoma", 7, 700);
+			Render.StringCustom(sx, sy, 0, "Developed by: Zeki", [255, 255, 255, 150], fonte);
+			Render.StringCustom(sx, sy + 20, 0, "GUI Library: ", [255, 255, 255, 150], fonte);
+			Render.StringCustom(sx + 65, sy + 20, 0, "Landry <3", [255, 0, 0, 150], fonte);
+		}
+		
 	}
-	if(getValue("Reset Amount")) reset();
-	if(getValue("Loopback")) {
-		UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", "Length", true);
-		if(playing && Math.abs(started + getValue("Length") - Globals.Realtime()) < 0.05){
+}
+
+// Frame Stage Notify
+function onFSN(){
+	if(loopback) {
+		if(playing && Math.abs(started + length[0] + 0.11 - Globals.Realtime()) < 0.05){
 			playing = false; Sound.StopMicrophone();
 			Cheat.ExecuteCommand("voice_loopback 0");
 		}
 	}
 }
 
+// Play Sound Function
 function playSound(){
-	if(!getValue("Enable KillVoice")) return;
+	if(!enabled) return;
 	if(!(Entity.GetEntityFromUserID(Event.GetInt("userid")) == Entity.GetLocalPlayer()) && Entity.GetEntityFromUserID(Event.GetInt("attacker")) == Entity.GetLocalPlayer()){
 		playing = true; started = Globals.Realtime();
-		if(getValue("Loopback")){
+		if(loopback){
 			Cheat.ExecuteCommand("voice_loopback 1");
 		}
-		var parsedName = "KillVoice " + (getValue("Selected")+1).toString();
-		Sound.PlayMicrophone('C:\\Program Files (x86)\\Steam\\steamapps\\common\\Counter-Strike Global Offensive\\' + UI.GetString("Misc", "JAVASCRIPT", "Script items", parsedName));
+		var parsedName = "KillVoice " + dropdownopts[chosenopt];
+		filenames = [file1, file2, file3, file4, file5];
+		if(!custom_directory) Sound.PlayMicrophone('C:\\Program Files (x86)\\Steam\\steamapps\\common\\Counter-Strike Global Offensive\\' + filenames[chosenopt] + '.wav');
+		else Sound.PlayMicrophone(directory + filenames[chosenopt] + '.wav');
 	}
 }
 
-function reset(){
-	UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", "KillVoice 1", false);
-	UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", "KillVoice 2", false);
-	UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", "KillVoice 3", false);
-	UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", "KillVoice 4", false);
-	UI.SetEnabled("Misc", "JAVASCRIPT", "Script items", "KillVoice 5", false);
-	setValue("Amount", 1);
-	setValue("Reset Amount", false);
-}
-
 Cheat.RegisterCallback("FrameStageNotify", "onFSN");
-Cheat.RegisterCallback("Unload", "reset");
 Cheat.RegisterCallback("player_death", "playSound");
+Cheat.RegisterCallback("Draw", "draw");
